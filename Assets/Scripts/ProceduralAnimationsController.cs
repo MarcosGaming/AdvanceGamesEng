4f27a4796f69ca6ee508c12e199a5fe4e03dce5c
MarcosGaming <40284094@live.napier.ac.uk>
2019-10-21 14:40:28 +0100

    Controller added

---
+++ Assets/Scripts/ProceduralAnimationsController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralAnimationsController : MonoBehaviour
+{
+    [SerializeField] Transform target;          // Target that the character is going to follow
+    [SerializeField] Stepper rightLegStepper;   // Right leg
+    [SerializeField] Stepper leftLegStepper;    // Left leg
+    [SerializeField] float turnSpeed;           // Turn speed
+    public float moveSpeed;                     // Movement speed
+    [SerializeField] float turnAcceleration;    // Turn acceleration
+    [SerializeField] float moveAcceleration;    // Movement acceleration
+    [SerializeField] float minDistToTarget;     // Min range to target
+    [SerializeField] float maxDistToTarget;     // Max range to target
+    [SerializeField] float maxAngToTarget;      // Max angle to target
+    Vector3 currentVelocity;                    // Current velocity
+    float currentAngularVelocity;               // Current angular velocity
+
+    private void Awake()
+    {
+        StartCoroutine(LegUpdateCoroutine());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        RootMotionUpdate();
+    }
+
+    // Coroutine for the leg movement
+    IEnumerator LegUpdateCoroutine()
+    {
+        // Run forever
+        while (true)
+        {
+            // Move right leg first
+            do
+            {
+                rightLegStepper.TryMove();
+                // Wait a frame 
+                yield return null;
+                // Stay in this loop while any of the legs try to move
+            } while (rightLegStepper.Moving);
+            yield return null;
+            // Move left leg second
+            do
+            {
+                leftLegStepper.TryMove();
+                // Wait a frame
+                yield return null;
+            } while (leftLegStepper.Moving);
+        }
+    }
+
+    // Movement
+    void RootMotionUpdate()
+    {
+        // Get direction towards target
+        Vector3 targetDir = target.position - transform.position;
+        // Vector toward target on the XZ plane
+        Vector3 targetDirProjected = Vector3.ProjectOnPlane(targetDir, transform.up);
+        // Angle from gecko forward direction toward our target
+        float angToTarget = Vector3.SignedAngle(transform.forward, targetDirProjected, transform.up);
+
+        float targetAngularVelocity = 0.0f;
+
+        // If within max angle leave angular velocity to zero
+        if (Mathf.Abs(angToTarget) > maxAngToTarget)
+        {
+            // Positive angle means right
+            if (angToTarget > 0)
+            {
+                targetAngularVelocity = turnSpeed;
+            }
+            else
+            {
+                targetAngularVelocity = -turnSpeed;
+            }
+        }
+
+        // Use smooth function to gradually change the velocity
+        currentAngularVelocity = Mathf.Lerp(currentAngularVelocity, targetAngularVelocity, 1 - Mathf.Exp(-turnAcceleration * Time.deltaTime));
+        // Rotate transform around the y axis
+        transform.Rotate(0, Time.deltaTime * currentAngularVelocity, 0, Space.World);
+
+        Vector3 targetVelocity = Vector3.zero;
+        // Dont move if gecko is not facing the target
+        if (Mathf.Abs(angToTarget) < 90)
+        {
+            float distToTarget = Vector3.Distance(transform.position, target.position);
+
+            // If gecko is too far away, approach the target
+            if (distToTarget > maxDistToTarget)
+            {
+                targetVelocity = moveSpeed * targetDirProjected.normalized;
+            }
+            // If gecko is too close reverse direction and move away
+            else if (distToTarget < minDistToTarget)
+            {
+                targetVelocity = moveSpeed * -targetDirProjected.normalized;
+            }
+        }
+
+        // Smooth velocity
+        currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, 1 - Mathf.Exp(-moveAcceleration * Time.deltaTime));
+
+        // Apply velocity
+        transform.position += currentVelocity * Time.deltaTime;
+    }
+}
--- Assets/Scripts/Stepper.cs
+++ Assets/Scripts/Stepper.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Stepper : MonoBehaviour
 {
 
-    [SerializeField] Transform target;      // Transform that is going to raytrace downwoards to the position the foot need to be at
-    [SerializeField] float stepAtDistance;  // Distance after which the leg is going to move
-    public bool Moving;                     // Checks if the leg is already moving
+    [SerializeField] Transform target;                          // Transform that is going to raytrace downwoards to the position the foot need to be at
+    [SerializeField] ProceduralAnimationsController mainBody;   // Get the velocity of the main body from here
+    [SerializeField] float stepAtDistance;                      // Distance after which the leg is going to move
+    [SerializeField] float stepTime;                            // Time that will take the leg to reach its position
+    public bool Moving;                                         // Checks if the leg is already moving
 
 
     // Start is called before the first frame update
@@ -24,8 +26,21 @@ public class Stepper : MonoBehaviour
         {
             Debug.DrawRay(target.position, Vector3.down * hit.distance, Color.red);
         }
+    }
+
+    private void LateUpdate()
+    {
+        TryMove();
+    }
+
+    // This function is called in the movement controller
+    public void TryMove()
+    {
+        // If already moving not start another move
         if (Moving) return;
-        if(Vector3.Distance(transform.position, target.position) > stepAtDistance)
+
+        // Start courotine when distance to target is greater or equal than the step distance
+        if (Vector3.Distance(transform.position, target.position) >= stepAtDistance)
         {
             StartCoroutine(MoveLeg());
         }
@@ -38,10 +53,10 @@ public class Stepper : MonoBehaviour
         // Starting position and rotation
         Vector3 startPos = transform.position;
         Quaternion startRot = transform.rotation;
-        //Final position and rotatuion
+        //Final position and rotation
         Vector3 endPos = new Vector3();
         Quaternion endRot = target.transform.rotation;
-        // The end positions is calculated by casting a ray downwards from the target (target is at the height of the knees and a bit ahead of them)
+        // The end positions is calculated by casting a ray downwards from the target 
         RaycastHit hit;
         if(Physics.Raycast(target.position,Vector3.down, out hit, 50.0f))
         {
@@ -51,25 +66,31 @@ public class Stepper : MonoBehaviour
         // Get middle position between initial and end positions
         Vector3 midPos = (startPos + endPos) / 2.0f;
         // Lift mid pos
-        midPos += target.up * Vector3.Distance(startPos, endPos) / 2.0f ;
+        midPos += Vector3.up * Vector3.Distance(startPos, endPos);
 
         // Distance from start position to end position
         float journeyLength = Vector3.Distance(startPos, endPos);
+        float journeyLengthPart1 = Vector3.Distance(startPos, midPos);
+        float journeyLengthPart2 = Vector3.Distance(midPos, endPos);
         // Time since the step started
         float timeElapsed = 0.0f;
-        // Distance covered by the lef/foot
-        float distanceCovered = 0.1f;
+        // The distance already covered by the leg
+        float distanceCovered = 0.0f;
         // Leg movement 
         do
         {
             timeElapsed += Time.deltaTime;
             // Calculate what distance still needs to be covered
-            distanceCovered = timeElapsed * 1.0f;
+            distanceCovered = timeElapsed * (mainBody.moveSpeed) * stepAtDistance;
             float fractionOfJourney = distanceCovered / journeyLength;
+            fractionOfJourney = Easing.Cubic.InOut(fractionOfJourney);
+            float fractionOfJouneyPart1 = distanceCovered / journeyLengthPart1;
+            fractionOfJouneyPart1 = Easing.Cubic.InOut(fractionOfJouneyPart1);
+            float fractionOfJourneyPart2 = distanceCovered / journeyLengthPart2;
+            fractionOfJourneyPart2 = Easing.Cubic.InOut(fractionOfJourneyPart2);
             // Interpolate position and rotation
-            transform.position = Vector3.Lerp(Vector3.Lerp(startPos, midPos, fractionOfJourney), Vector3.Lerp(midPos, endPos, fractionOfJourney), fractionOfJourney);
-            transform.rotation = Quaternion.Slerp(startRot, endRot, fractionOfJourney);
-            print(fractionOfJourney);
+            transform.position = Vector3.Lerp(Vector3.Lerp(startPos, midPos, fractionOfJouneyPart1), Vector3.Lerp(midPos, endPos, fractionOfJourneyPart2), fractionOfJourney);
+            transform.rotation = Quaternion.Slerp(startRot, endRot, fractionOfJourney); 
             // Wait one frame
             yield return null;
         } while (distanceCovered < journeyLength);
